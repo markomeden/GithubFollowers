11eb3bf683444c921d2f3451d4dc5b8b509513da
markomeden <marko.medenn@gmail.com>
2024-02-25 15:01:06 +0100

    First steps, first screen
    Added assets, removed default view controller, added custom button, added custom text field, modified scene delegate, modified searchVC.

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesVC.swift
+//  GithubFollowers
+//
+//  Created by Marko Meden on 25. 02. 24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
