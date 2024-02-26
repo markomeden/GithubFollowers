d3d831f8b99f26929f1a859c3aed065fdc8fe753
markomeden <marko.medenn@gmail.com>
2024-02-26 18:11:09 +0100

    GFAlertVC
    Added alert view controller, extension. One instance shows when username is empty.

---
+++ GithubFollowers/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  GithubFollowers
+//
+//  Created by Marko Meden on 26. 02. 24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle  = .overFullScreen
+            alertVC.modalTransitionStyle    = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
