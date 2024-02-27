c9d5ae4fbd1525e7301b11b2739ac9717b55f313
markomeden <marko.medenn@gmail.com>
2024-02-27 15:07:56 +0100

    Network Calls, Error Handling, Architecture

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Marko Meden on 26. 02. 24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
